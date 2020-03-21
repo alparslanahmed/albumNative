6ac2f88fe92906de3cc82feac7be1c3d6cce105d
Ahmed Alparslan Özdemir <alparslanahmed@gmail.com>
2020-03-21 21:38:16 +0300

    Login updates

--- Screens/Login/index.js
+++ Screens/Login/index.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
 import {Button, Card, Divider, Input, Layout, Text, TopNavigation} from '@ui-kitten/components';
-import {SafeAreaView, Image} from 'react-native';
+import {KeyboardAvoidingView, StyleSheet, View, ScrollView, Image} from 'react-native';
 import {connect} from 'react-redux';
 import {loginRequest} from './actions';
-import logo from '../../assets/img/logo.png';
+import {
+  EyeIcon,
+  EyeOffIcon,
+  FacebookIcon,
+  GoogleIcon,
+  PersonIcon,
+  TwitterIcon,
+} from '../../components/icons';
+import {ImageOverlay} from '../../components/image-overlay.component';
+import loginImage from '../../assets/img/login.jpg';
+import logoImage from '../../assets/img/logo_white.png';
 
 export function LoginScreen({login, dispatch, navigation}) {
 
-  console.log('LOGIN SCREEN!');
-
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [passwordVisible, setPasswordVisible] = React.useState(false);
 
   const passwordInput = React.useRef();
 
@@ -18,40 +27,110 @@ export function LoginScreen({login, dispatch, navigation}) {
     dispatch(loginRequest(email, password));
   }
 
+  const onSignInButtonPress = () => {
+    navigation && navigation.goBack();
+  };
+
+  const onSignUpButtonPress = () => {
+    navigation && navigation.navigate('SignUp4');
+  };
+
+  const onForgotPasswordButtonPress = () => {
+    navigation && navigation.navigate('ForgotPassword');
+  };
+
+  const onPasswordIconPress = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Divider/>
-      <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20}}>
-        <Image
-          style={{height: 100, resizeMode: 'contain'}}
-          source={logo}
-        />
-        <Input
-          placeholder='Email'
-          value={email}
-          onChangeText={setEmail}
-          textContentType={'emailAddress'}
-          autoCompleteType={'email'}
-          keyboardType={'email-address'}
-          onSubmitEditing={() => {
-            passwordInput.current.focus();
-          }}
-          blurOnSubmit={false}
-          autoCapitalize={'none'}
-        />
-        <Input
-          ref={passwordInput}
-          placeholder='Password'
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry={true}
-          onSubmitEditing={submitForm}
-        />
-        <Button onPress={submitForm} style={{width: '100%'}}>
-          Login
+    <ImageOverlay
+      style={styles.container}
+      source={loginImage}>
+      <ScrollView style={{flex: 1}}>
+        <View style={styles.headerContainer}>
+          <Image source={logoImage} style={{height: 150, resizeMode: 'contain'}}/>
+          <Text
+            style={styles.signInLabel}
+            category='s1'
+            status='control'>
+            Sign in to your account
+          </Text>
+        </View>
+        <View style={styles.formContainer}>
+          <Input
+            placeholder='Email'
+            value={email}
+            onChangeText={setEmail}
+            textContentType={'emailAddress'}
+            autoCompleteType={'email'}
+            keyboardType={'email-address'}
+            onSubmitEditing={() => {
+              passwordInput.current.focus();
+            }}
+            blurOnSubmit={false}
+            autoCapitalize={'none'}
+          />
+          <Input
+            ref={passwordInput}
+            placeholder='Password'
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={true}
+            onSubmitEditing={submitForm}
+          />
+          <View style={styles.forgotPasswordContainer}>
+            <Button
+              style={styles.forgotPasswordButton}
+              appearance='ghost'
+              status='control'
+              onPress={onForgotPasswordButtonPress}>
+              Forgot your password?
+            </Button>
+          </View>
+        </View>
+        <Button
+          style={styles.signInButton}
+          size='giant'
+          onPress={onSignInButtonPress}>
+          SIGN IN
         </Button>
-      </Layout>
-    </SafeAreaView>
+        <View style={styles.socialAuthContainer}>
+          <Text
+            style={styles.socialAuthHintText}
+            status='control'>
+            Or Sign In using Social Media
+          </Text>
+          <View style={styles.socialAuthButtonsContainer}>
+            <Button
+              appearance='ghost'
+              status='control'
+              size='giant'
+              icon={GoogleIcon}
+            />
+            <Button
+              appearance='ghost'
+              status='control'
+              size='giant'
+              icon={FacebookIcon}
+            />
+            <Button
+              appearance='ghost'
+              status='control'
+              size='giant'
+              icon={TwitterIcon}
+            />
+          </View>
+        </View>
+        <Button
+          style={styles.signUpButton}
+          appearance='ghost'
+          status='control'
+          onPress={onSignUpButtonPress}>
+          Don't have an account? Sign Up
+        </Button>
+      </ScrollView>
+    </ImageOverlay>
   );
 }
 
@@ -70,3 +149,48 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerContainer: {
+    minHeight: 216,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  formContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  signInLabel: {
+    marginTop: 16,
+  },
+  passwordInput: {
+    marginTop: 16,
+  },
+  signInButton: {
+    marginHorizontal: 16,
+  },
+  forgotPasswordContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  forgotPasswordButton: {
+    paddingHorizontal: 0,
+  },
+  signUpButton: {
+    marginVertical: 12,
+  },
+  socialAuthContainer: {
+    marginTop: 32,
+  },
+  socialAuthButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  socialAuthHintText: {
+    alignSelf: 'center',
+    marginBottom: 16,
+  },
+});
